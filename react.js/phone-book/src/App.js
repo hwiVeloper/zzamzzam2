7668121e75742216aa65a9c815a007ddf69d23c8
Jonghwi Lee <zziller03@gmail.com>
2018-07-30 17:39:51 +0900

    [React.js] 불변성에 관하여

--- react.js/phone-book/src/App.js
+++ react.js/phone-book/src/App.js
@@ -18,7 +18,13 @@ class App extends Component {
                 name: '홍길서',
                 phone: '010-0000-0001'
             }
-        ]
+        ],
+        keyword: ''
+    }
+    handleChange = (e) => {
+        this.setState({
+            keyword: e.target.value,
+        });
     }
     handleCreate = (data) => {
         const { information } = this.state;
@@ -43,12 +49,23 @@ class App extends Component {
         })
     }
     render() {
-        const { information } = this.state;
+        const { information, keyword } = this.state;
+        const filteredList = information.filter(
+            info => info.name.indexOf(keyword) !== -1
+        );
         return (
             <div>
                 <PhoneForm onCreate={this.handleCreate}/>
+                <p>
+                    <input
+                        placeholder="검색어를 입력하세요."
+                        onChange={this.handleChange}
+                        value={keyword}
+                    />
+                </p>
+                <hr />
                 <PhoneInfoList
-                    data={information}
+                    data={filteredList}
                     onRemove={this.handleRemove}
                     onUpdate={this.handleUpdate}/>
             </div>
