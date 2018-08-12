7f981a4d59867ade1e88f44f3a8a8754dd1765ec
Jonghwi Lee <zziller03@gmail.com>
2018-08-12 13:15:30 +0900

    [React.js] todo-list 실습완성
    * 다음은 추가적으로 Palette 컴포넌트를 만들어본다

--- [React.js] Velopert님 강의.js/todo-list/src/components/TodoItemList.js
+++ [React.js] Velopert님 강의.js/todo-list/src/components/TodoItemList.js
@@ -2,14 +2,29 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.todos !== nextProps.todos;
+    }
+
     render () {
         const { todos, onToggle, onRemove } = this.props;
 
+        const todoList = todos.map(
+            ({id, text, checked}) => (
+                <TodoItem
+                    id={id}
+                    text={text}
+                    checked={checked}
+                    onToggle={onToggle}
+                    onRemove={onRemove}
+                    key={id}
+                />
+            )
+        );
+
         return (
             <div>
-                <TodoItem text="안녕 반가워"/>
-                <TodoItem text="이건 리액트야"/>
-                <TodoItem text="어때?"/>
+                {todoList}
             </div>
         );
     }
