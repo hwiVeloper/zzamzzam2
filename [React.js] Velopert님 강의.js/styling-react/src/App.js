2694cd957e15c515de9abef0b772195cc98cc8c7
Jonghwi Lee <zziller03@gmail.com>
2018-08-15 18:25:57 +0900

    [React.js] react styling: scss사용법 익히기

--- [React.js] Velopert님 강의.js/styling-react/src/App.js
+++ [React.js] Velopert님 강의.js/styling-react/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames/bind';
+//import styles from './App.css';
+import styles from './App.scss';
+
+console.log(styles); // 확인용
+
+const cn = classNames.bind(styles);
 
 class App extends Component {
   render() {
+    const isBlue = false;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // <div className={classNames(styles.box, styles.blue)}>
+      <div className={cn('box', {blue: isBlue})}>
+        <div className={cn('box-inside')}/>
       </div>
     );
   }
