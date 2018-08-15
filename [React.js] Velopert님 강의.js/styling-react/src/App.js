ff89cab491fe4ef8a346599e1a45ed49dd6a5ce1
Jonghwi Lee <zziller03@gmail.com>
2018-08-15 19:48:25 +0900

    [React.js] react styled-component 실습

--- [React.js] Velopert님 강의.js/styling-react/src/App.js
+++ [React.js] Velopert님 강의.js/styling-react/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import classNames from 'classnames/bind';
-//import styles from './App.css';
-import styles from './App.scss';
-
-console.log(styles); // 확인용
-
-const cn = classNames.bind(styles);
+import Button from './components/Button';
+import StyledButton from './components/StyledButton';
 
 class App extends Component {
   render() {
     const isBlue = false;
     return (
-      // <div className={classNames(styles.box, styles.blue)}>
-      <div className={cn('box', {blue: isBlue})}>
-        <div className={cn('box-inside')}/>
+      <div>
+        <Button>Button</Button>
+        <StyledButton big>StyledButton</StyledButton>
       </div>
     );
   }
