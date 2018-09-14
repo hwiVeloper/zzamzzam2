16260b882ae04970ec811aeb1604b72e1cef0c35
Jonghwi Lee <zziller03@gmail.com>
2018-09-15 01:59:43 +0900

    jsx 기본

---
+++ [React.js] react-quickly/ch03/hello-world-jsx-class/js/script.jsx
@@ -0,0 +1,15 @@
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>1. Hello world!</h1>
+                <h1>2. Hello world!!</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById('content')
+)
---
+++ [React.js] react-quickly/ch03/hello-world-jsx/js/script.jsx
@@ -0,0 +1,6 @@
+ReactDOM.render(
+    // React.createElement('h1', null, 'Hello World'),
+    // document.getElementById('content')
+    <h1>Hello world!</h1>,
+    document.getElementById('content')
+)
