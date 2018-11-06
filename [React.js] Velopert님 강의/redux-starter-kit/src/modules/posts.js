57251382f6320c7edb7c143ae28ea1750cdcb62e
Jonghwi Lee <zziller03@gmail.com>
2018-11-06 10:44:20 +0900

    redux 미들웨어
    - redux-promise-middleware
    - reudx-pending

--- [React.js] Velopert님 강의/redux-starter-kit/src/modules/posts.js
+++ [React.js] Velopert님 강의/redux-starter-kit/src/modules/posts.js
@@ -1,4 +1,5 @@
-import {handleActions} from 'redux-actions';
+import {createAction, handleActions} from 'redux-actions';
+import {pender} from 'redux-pender';
 
 import axios from 'axios';
 
@@ -6,29 +7,42 @@ function getPostAPI(postId) {
     return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
 }
 
-const GET_POST_PENDING = 'GET_POST_PENDING';
-const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'GET_POST_FAILURE';
+const GET_POST = 'GET_POST';
+// const GET_POST_PENDING = 'GET_POST_PENDING';
+// const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+// const GET_POST_FAILURE = 'GET_POST_FAILURE';
 
-export const getPost = (postId) => dispatch => {
-    // 요청이 시작함을 알림
-    dispatch({type: GET_POST_PENDING});
+/*
+    redux-pender 의 액션 구조는 Flux standard action(https://github.com/acdlite/flux-standard-action) 
+    을 따르기 때문에, createAction 으로 액션을 생성 할 수 있습니다. 두번째로 들어가는 파라미터는 프로미스를 반환하는
+    함수여야 합니다.
+*/
+export const getPost = createAction(GET_POST, getPostAPI);
 
-    // 요청 시작
-    return getPostAPI(postId).then(
-        (response) => {
-            dispatch({
-                type: GET_POST_SUCCESS,
-                payload: response
-            })
-        }
-    ).catch(error => {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: error
-        });
-    });
-}
+// export const getPost = (postId) => ({
+//     type: GET_POST,
+//     payload: getPostAPI(postId)
+// })
+
+// export const getPost = (postId) => dispatch => {
+//     // 요청이 시작함을 알림
+//     dispatch({type: GET_POST_PENDING});
+
+//     // 요청 시작
+//     return getPostAPI(postId).then(
+//         (response) => {
+//             dispatch({
+//                 type: GET_POST_SUCCESS,
+//                 payload: response
+//             })
+//         }
+//     ).catch(error => {
+//         dispatch({
+//             type: GET_POST_FAILURE,
+//             payload: error
+//         });
+//     });
+// }
 
 const initialState = {
     pending: false,
@@ -40,29 +54,49 @@ const initialState = {
 }
 
 export default handleActions({
-    [GET_POST_PENDING]: (state, action) => {
-        return {
-            ...state,
-            pending: true,
-            error: false
-        };
-    },
-    [GET_POST_SUCCESS]: (state, action) => {
-        const {title, body} = action.payload.data;
-
-        return {
-            ...state,
-            pending: false,
-            data: {
-                title, body
+    ...pender({
+        type: GET_POST,
+        /*
+            요청중 / 실패 했을 때 추가적으로 해야 할 작업이 있다면 이렇게 onPending 과 onFailure 를 추가해주면됩니다.
+            onPending: (state, action) => state,
+            onFailure: (state, action) => state
+        */
+        onSuccess: (state, action) => {
+            const {title, body} = action.payload.data;
+            return {
+                data: {
+                    title, body
+                }
             }
-        };
-    },
-    [GET_POST_FAILURE]: (state, action) => {
-        return {
-            ...state,
-            pending: false,
-            error: true
         }
-    }
+    })
 }, initialState);
+
+
+// export default handleActions({
+//     [GET_POST_PENDING]: (state, action) => {
+//         return {
+//             ...state,
+//             pending: true,
+//             error: false
+//         };
+//     },
+//     [GET_POST_SUCCESS]: (state, action) => {
+//         const {title, body} = action.payload.data;
+
+//         return {
+//             ...state,
+//             pending: false,
+//             data: {
+//                 title, body
+//             }
+//         };
+//     },
+//     [GET_POST_FAILURE]: (state, action) => {
+//         return {
+//             ...state,
+//             pending: false,
+//             error: true
+//         }
+//     }
+// }, initialState);
--- [React.js] Velopert님 강의/redux-starter-kit/src/store.js
+++ [React.js] Velopert님 강의/redux-starter-kit/src/store.js
@@ -4,13 +4,18 @@ import modules from './modules';
 
 import {createLogger} from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+// import promiseMiddleware from 'redux-promise-middleware';
+import penderMiddleware from 'redux-pender';
 
 /*
     로그 미들웨어를 생성할 때 설정을 커스터마이징 할 수 있다.
     https://github.com/evgenyrodionov/redux-logger#options 참조
 */
 const logger = createLogger();
+// const customizedPromiseMiddleware = promiseMiddleware({
+//     promiseTypeSuffixes: ['LOADING', 'SUCCESS', 'FAILURE']
+// });
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk, penderMiddleware()));
 
 export default store;
