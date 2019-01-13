4ee0583b860854868ec721e8c5000acae6a4b04b
Jonghwi Lee <zziller03@gmail.com>
2019-01-14 01:25:03 +0900

    ch17 마무리 - mongodb, express, react

--- [React.js] react-quickly/ch17/message-board/index.js
+++ [React.js] react-quickly/ch17/message-board/index.js
@@ -10,7 +10,7 @@ const express = require("express"),
   logger = require("morgan"),
   errorHandler = require("errorhandler"),
   compression = require("compression"),
-  url = "mongodb://ds161016.mlab.com:61016/react-study",
+  url = "mongodb://hwiveloper:paper0817@ds161016.mlab.com:61016/react-study",
   ReactDOMServer = require("react-dom/server"),
   React = require("react");
 
@@ -25,6 +25,8 @@ mongodb.MongoClient.connect(
       console.error(err);
       process.exit(1);
     }
+
+    console.log("Connected");
 
     app.set("view engine", "hbs");
 
@@ -42,15 +44,41 @@ mongodb.MongoClient.connect(
     });
 
     app.get("/messages", (req, res, next) => {
-      //
+      req.messages.find({}, { sort: { _id: -1 } }).toArray((err, docs) => {
+        if (err) return next(err);
+        return res.json(docs);
+      });
     });
 
     app.post("/messages", (req, res, next) => {
-      //
+      req.checkBody("message", "Invalid message in body").notEmpty();
+      req.checkBody("name", "Invalid name in body").notEmpty(); // 요청 내용에 있는 메세지 확인
+      var errors = req.validationErrors();
+      if (errors) return next(errors);
+      req.messages.insert(req.body, (err, result) => {
+        // 요청 내용을 데이터베이스에 추가
+        if (err) return next(err);
+        return res.json(result.ops[0]); // 데이터베이스에서 자동 생성된 새로운 문서의 id를 출력
+      });
     });
 
     app.get("/", (req, res, next) => {
-      //
+      req.messages.find({}, { sort: { _id: -1 } }).toArray((err, docs) => {
+        if (err) return next(err);
+        res.render("index", {
+          header: ReactDOMServer.renderToString(Header()),
+          footer: ReactDOMServer.renderToString(Footer()),
+          messageBoard: ReactDOMServer.renderToString(
+            MessageBoard({
+              messages: docs
+            })
+          ),
+          props:
+            '<script type="text/javascript">var messages=' +
+            JSON.stringify(docs) +
+            "</script>"
+        });
+      });
     });
 
     app.listen(3000);
