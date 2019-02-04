cca54e45bbad78d1d6c8a3aa2eaff04fc18fcd5a
Jonghwi Lee <zziller03@gmail.com>
2019-02-04 19:54:20 +0900

    ch19 비밀번호 생성 테스트 (Jest)

---
+++ [React.js] react-quickly/ch19/password/__tests__/generate-password.test.js
@@ -0,0 +1,19 @@
+const generatePassword = require("../js/generate-password.js");
+const pattern = /^[A-Za-z0-9\!\@\#\$\%\^\&\*\(\)\_\+\{\}\:\"\<\>\?\\|\[\]\/'\,\.\`\~]{8,16}$/;
+
+describe("Method generatePassword", () => {
+  let password, password2;
+
+  it("설정한 패턴에 맞는 비밀번호를 반환한다.", () => {
+    password = generatePassword();
+    console.log(password);
+    expect(password).toMatch(pattern);
+  });
+
+  it("이전에 생성한 비밀번호와 다른 새로운 비밀번호를 반환한다.", () => {
+    password2 = generatePassword();
+    console.log(password2);
+    expect(password2).toMatch(pattern);
+    expect(password2).not.toEqual(password);
+  });
+});
